9ad7b821db6f61adbdb5f2c0794c93143c2fbd68
shinyoungkim30 <ksyq12@gmail.com>
2023-06-26 13:36:35 +0900

    찜리스트 뱃지 추가하기 전 파일

--- react/src/components/CombinationPrice.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CombinationPrice = () => {
-  return (
-    <div>CombinationPrice</div>
-  )
-}
-
-export default CombinationPrice
--- react/src/components/SurveyResult.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SurveyResult = () => {
-  return (
-    <div>SurveyResult</div>
-  )
-}
-
-export default SurveyResult
