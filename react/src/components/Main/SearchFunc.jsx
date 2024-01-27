a0c7b76989c13fa55ea5ef7ae6e58e69ae6ec056
seohyunx <sh0501sh11@gmail.com>
2024-01-27 23:00:12 +0900

    Clone HaruGungang Project

--- react/src/components/Main/SearchFunc.jsx
+++ react/src/components/Main/SearchFunc.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
-import { Row, Col, Container, Card, CardBody } from 'reactstrap';
-import DetailFunc from './DetailFunc';
-import FloatDetailFunc from './FloatDetailFunc';
-const SearchFunc = ({title, setTitle, nutri, setNutri, effect, setEffect, func, btnClick, visible, setVisible}) => {
+import React from "react";
+import { Row, Col, Container, Card, CardBody } from "reactstrap";
+import DetailFunc from "./DetailFunc";
+import FloatDetailFunc from "./FloatDetailFunc";
+const SearchFunc = ({
+  title,
+  setTitle,
+  nutri,
+  setNutri,
+  effect,
+  setEffect,
+  func,
+  btnClick,
+  visible,
+  setVisible,
+}) => {
   return (
     <div>
-
-<<<<<<< HEAD:templete/src/components/Main/SearchFunc.jsx
-    <Container>
-      <div className='box-container'>
-        <div className="justify-content-center">
-            <Col md="7" className="text-center">
-              <br/>
-                <h2 className="title font-bold">기능성으로 정보 찾기</h2>
-=======
-    <Container className='m-b-40'>
-      <div className='box-container'>
-        <div className="justify-content-center">
+      <Container className="m-b-40">
+        <div className="box-container">
+          <div className="justify-content-center">
             <Col md="5" className="text-center">
-              <br/>
-                <h2 className="title font-bold m-t-40 text-decoration">기능성으로 정보 찾기</h2>
->>>>>>> ksy:react/src/components/Main/SearchFunc.jsx
+              <br />
+              <h2 className="title font-bold m-t-40 text-decoration">
+                기능성으로 정보 찾기
+              </h2>
             </Col>
+          </div>
+          <div className="detailFunc_container">
+            <Row className="m-t-40">
+              <DetailFunc
+                title={title}
+                setTitle={setTitle}
+                nutri={nutri}
+                setNutri={setNutri}
+                effect={effect}
+                setEffect={setEffect}
+                func={func}
+                btnClick={btnClick}
+                visible={visible}
+                setVisible={setVisible}
+              />
+            </Row>
+          </div>
         </div>
-        <div className='detailFunc_container'>
-        <Row className="m-t-40">
-        <DetailFunc 
-        title={title}
-        setTitle={setTitle}
-        nutri={nutri}
-        setNutri={setNutri}
-        effect={effect}
-        setEffect={setEffect}
-        func={func} btnClick={btnClick} visible={visible} setVisible={setVisible} />
-        </Row>
-        </div>
-        </div>
-    </Container>
-
-
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default SearchFunc
+export default SearchFunc;
